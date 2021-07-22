0fbb2f29835581d4875f49f1d6f35d7ea482068f
cenk aksu <83034441+cenk314315@users.noreply.github.com>
2021-07-22 19:28:58 +0300

    Create odev10.sql

---
+++ odev10.sql
@@ -0,0 +1,18 @@
+-- 1 city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
+
+Select city, country FROM city
+LEFT JOIN country ON country.country_id = city.country_id
+
+-- 2 customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
+
+Select first_name, last_name FROM customer
+RIGHT JOIN rental ON customer.customer_id = rental.customer_id
+
+-- 3 customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz
+FULL JOIN sorgusunu yazınız.
+
+Select first_name, last_name FROM customer
+FULL JOIN rental ON customer.customer_id = rental.customer_id
+
+
+
